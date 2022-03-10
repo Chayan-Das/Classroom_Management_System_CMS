3e96a11cc9688cf3c7a5b5342743a64842fecce1
dasch <10surjodas10@gmail.com>
2022-03-10 21:16:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/classroommanagementsystemcms/SplashScreen.java
@@ -0,0 +1,89 @@
+package com.example.classroommanagementsystemcms;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5920;
+
+    //variables
+
+    LottieAnimationView lottieAnimationView;
+    TextView nointernet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        lottieAnimationView = findViewById(R.id.loading_splash);
+        nointernet = findViewById(R.id.no_internet);
+
+
+        //lottieAnimationView.animate().translationX(-1600).setDuration(300).setStartDelay(5920);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                //startActivity(intent);
+                //finish();
+
+
+                show();
+
+            }
+        },SPLASH_SCREEN);
+
+    }
+
+    private void show() {
+
+
+
+        if(isConnected(this)){
+
+            lottieAnimationView.setVisibility(View.INVISIBLE);
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        else {
+            nointernet.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
+    private boolean isConnected(SplashScreen splashScreen) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) splashScreen.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wificon = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobiledata = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if((wificon != null && wificon.isConnected()) || mobiledata != null && mobiledata.isConnected())
+        {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/classroommanagementsystemcms/account/Signup.java
@@ -0,0 +1,16 @@
+package com.example.classroommanagementsystemcms.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.classroommanagementsystemcms.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
