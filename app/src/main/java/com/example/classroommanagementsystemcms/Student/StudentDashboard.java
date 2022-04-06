b235cf32b93dbc56df62055eb8ba2cdcf26950ea
Chayan-Das <10surjodas10@gmail.com>
2022-04-06 22:17:19 +0600

    28/03

--- app/src/main/java/com/example/classroommanagementsystemcms/Student/StudentDashboard.java
+++ app/src/main/java/com/example/classroommanagementsystemcms/Student/StudentDashboard.java
@@ -20,27 +20,7 @@ public class StudentDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_student_dashboard);
 
         r1=findViewById(R.id.app_bar);
-        r2=findViewById(R.id.show_more);
 
-        /*
 
-        up_down.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(r2.getVisibility() == View.VISIBLE){
-                    r2.setVisibility(View.GONE);
-                    up_down.setRotation(180);
-
-                }
-                else {
-                    r2.setVisibility(View.VISIBLE);
-
-                }
-
-            }
-        });
-
-         */
     }
 }
